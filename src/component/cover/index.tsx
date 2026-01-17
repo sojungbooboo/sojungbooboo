70aefa88240e8de2dd6dff8c89e8bf023f6b08f7
thisisyjin <hnesb970@gmail.com>
2026-01-17 22:16:17 +0900

    랜딩 페이지에 We get Married! 붓글씨 인트로 애니메이션 추가

--- src/component/cover/index.tsx
+++ src/component/cover/index.tsx
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react"
 import "./index.scss"
 
 export const Cover = () => {
+  const [showIntro, setShowIntro] = useState(true)
+
+  useEffect(() => {
+    // 3초 후 인트로 오버레이 사라지기
+    const timer = setTimeout(() => {
+      setShowIntro(false)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className="cover">
+      {/* 인트로 오버레이 */}
+      {showIntro && (
+        <div className="intro-overlay">
+          <div className="intro-text">
+            <span className="brush-text">We get Married!</span>
+          </div>
+        </div>
+      )}
       <div className="cover-image">
         <img 
           src="https://images.unsplash.com/photo-1519741497674-611481863552?w=800&q=80" 
