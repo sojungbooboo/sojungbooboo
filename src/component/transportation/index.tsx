49367cd0ac67733453bf070e6cc0637d2f168db8
Yeonjin Lee <hnesb970@gmail.com>
2026-01-29 15:13:48 +0900

    transportation-section: 버스/지하철/무료셔틀버스/주차 영역 스타일 개선

--- src/component/transportation/index.tsx
+++ src/component/transportation/index.tsx
@@ -34,7 +34,11 @@ export const Transportation = () => {
           <h3>지하철</h3>
           <div className='transport-boxes'>
             <div className='transport-box'>
-              <p className='line'>1호선 대방역 (마을버스)</p>
+              <p className='line'>
+                <span className='subway-number subway-number--line1'>1</span>
+                <span className='subway-station'>대방역</span>
+                <span className='subway-note'>마을버스</span>
+              </p>
               <p className='detail'>
                 대방역(19253정류장)
                 <br />
@@ -42,7 +46,11 @@ export const Transportation = () => {
               </p>
             </div>
             <div className='transport-box'>
-              <p className='line'>7호선 신풍역 (지선버스)</p>
+              <p className='line'>
+                <span className='subway-number subway-number--line7'>7</span>
+                <span className='subway-station'>신풍역</span>
+                <span className='subway-note'>지선버스</span>
+              </p>
               <p className='detail'>
                 3번 출구(19219정류장)
                 <br />
@@ -50,7 +58,11 @@ export const Transportation = () => {
               </p>
             </div>
             <div className='transport-box'>
-              <p className='line'>7호선 보라매역 (도보)</p>
+              <p className='line'>
+                <span className='subway-number subway-number--line7'>7</span>
+                <span className='subway-station'>보라매역</span>
+                <span className='subway-note'>도보</span>
+              </p>
               <p className='detail'>
                 5번 출구에서 200m 직진 후
                 <br />
@@ -58,7 +70,11 @@ export const Transportation = () => {
               </p>
             </div>
             <div className='transport-box'>
-              <p className='line'>신림선 서울지방병무청역 (도보)</p>
+              <p className='line'>
+                <span className='subway-number subway-number--sillim'>신림</span>
+                <span className='subway-station'>서울지방병무청역</span>
+                <span className='subway-note'>도보</span>
+              </p>
               <p className='detail'>
                 2번 출구에서 100m 직진 후
                 <br />
@@ -69,7 +85,12 @@ export const Transportation = () => {
         </div>
 
         <div className='transport-section'>
-          <h3>무료셔틀버스</h3>
+          <div className='section-header'>
+            <h3>무료셔틀버스</h3>
+            <p className='shuttle-note'>
+              예식시간 1시간 30분 전부터 10~15분 간격 운행
+            </p>
+          </div>
           <div className='transport-boxes'>
             <div className='transport-box'>
               <p className='line'>1호선 대방역 3번 출구</p>
@@ -78,18 +99,16 @@ export const Transportation = () => {
               <p className='line'>7호선 보라매역 5번 출구</p>
             </div>
           </div>
-          <p className='shuttle-note'>
-            (예식시간 1시간 30분 전부터 10~15분 간격 운행)
-          </p>
         </div>
 
         <div className='transport-section'>
-          <h3>주차</h3>
+          <div className='section-header'>
+            <h3>주차</h3>
+            <p className='parking-note'>2시간 무료</p>
+          </div>
           <div className='transport-box'>
             <p className='detail'>
               주차직원 안내에 따라 해군호텔 W웨딩홀 주차장 이용
-              <br />
-              <span className='parking-highlight'>(2시간 무료)</span>
             </p>
           </div>
         </div>
