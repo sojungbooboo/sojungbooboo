ff2914553b39ce04b10d8a2a4036b0db6bd519a5
Yeonjin Lee <hnesb970@gmail.com>
2026-02-04 10:51:25 +0900

    couple-info: 생년월일 제거, 부모님 이름 폰트 크기 및 볼드 적용

--- src/component/coupleInfo/index.tsx
+++ src/component/coupleInfo/index.tsx
@@ -1,12 +1,10 @@
 import {
   GROOM_FULLNAME,
   GROOM_ENGLISH_NAME,
-  GROOM_BIRTHDATE,
   GROOM_FATHER,
   GROOM_MOTHER,
   BRIDE_FULLNAME,
   BRIDE_ENGLISH_NAME,
-  BRIDE_BIRTHDATE,
   BRIDE_FATHER,
   BRIDE_MOTHER,
 } from "../../const"
@@ -18,11 +16,10 @@ export const CoupleInfo = () => {
       <div className="content">
         <div className="groom">
           <div className="parents">
-            {GROOM_FATHER}, {GROOM_MOTHER}의 아들
+            <span className="parent-name">{GROOM_FATHER}</span>, <span className="parent-name">{GROOM_MOTHER}</span>의 아들
           </div>
           <div className="name-section">
             <div className="korean-name">신랑 {GROOM_FULLNAME}</div>
-            <div className="birthdate">{GROOM_BIRTHDATE}</div>
             <div className="english-name">{GROOM_ENGLISH_NAME}</div>
           </div>
         </div>
@@ -31,11 +28,10 @@ export const CoupleInfo = () => {
 
         <div className="bride">
           <div className="parents">
-            {BRIDE_FATHER}, {BRIDE_MOTHER}의 딸
+            <span className="parent-name">{BRIDE_FATHER}</span>, <span className="parent-name">{BRIDE_MOTHER}</span>의 딸
           </div>
           <div className="name-section">
             <div className="korean-name">신부 {BRIDE_FULLNAME}</div>
-            <div className="birthdate">{BRIDE_BIRTHDATE}</div>
             <div className="english-name">{BRIDE_ENGLISH_NAME}</div>
           </div>
         </div>
